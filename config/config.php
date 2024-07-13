cbaaeb0677e1a7127f53381fcdc9d323ae1a570e
Berro-1 <hussienberro1@gmail.com>
2024-07-13 15:34:34 +0300

    added database configuration

---
+++ config/config.php
@@ -0,0 +1,22 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'chris_airlines_db');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+function getDBConnection()
+{
+    $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection error: " . $conn->connect_error);
+    }
+    
+    echo "MySQLi connection successfully established.<br>";
+    
+    return $conn;
+}
+
+$mysqli = getDBConnection();
+
