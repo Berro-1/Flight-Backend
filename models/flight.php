3b2e5f71473f13ac28419e2591de529bf52e4450
Ali-Hadi-Saker <alihady530@gmail.com>
2024-07-16 01:51:37 +0300

    added flight search

--- models/flight.php
+++ models/flight.php
@@ -74,13 +74,13 @@ class Flight
 
     public function getFlights($departure_airport_id, $destination_airport_id)
     {
-        $query = 'SELECT * FROM flights WHERE departure_airport_id = ? AND destination_airport_id = ? AND departure_datetime >= ? AND arrival_datetime <= ? ';
+        $query = 'SELECT * FROM flights WHERE departure_airport_id = ? AND destination_airport_id = ?';
         $stmt = $this->mysqli->prepare($query);
         if (!$stmt) {
             return ["message" => "Database error: " . $this->mysqli->error];
         }
     
-        $stmt->bind_param("iiss", $departure_airport_id, $destination_airport_id);
+        $stmt->bind_param("ii", $departure_airport_id, $destination_airport_id);
         $stmt->execute();
         $result = $stmt->get_result();
         if (!$result) {
