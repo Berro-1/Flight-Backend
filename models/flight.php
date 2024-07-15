f43b2f0008e7b22f336bbfe1de33ca14a1ad7f7a
Berro-1 <hussienberro1@gmail.com>
2024-07-15 21:42:08 +0300

    adjusted get specific flights api

--- models/flight.php
+++ models/flight.php
@@ -72,7 +72,7 @@ class Flight
         }
     }
 
-    public function getFlights($departure_airport_id, $destination_airport_id, $departure_datetime, $arrival_datetime)
+    public function getFlights($departure_airport_id, $destination_airport_id)
     {
         $query = 'SELECT * FROM flights WHERE departure_airport_id = ? AND destination_airport_id = ? AND departure_datetime >= ? AND arrival_datetime <= ? ';
         $stmt = $this->mysqli->prepare($query);
@@ -80,7 +80,7 @@ class Flight
             return ["message" => "Database error: " . $this->mysqli->error];
         }
     
-        $stmt->bind_param("iiss", $departure_airport_id, $destination_airport_id, $departure_datetime, $arrival_datetime);
+        $stmt->bind_param("iiss", $departure_airport_id, $destination_airport_id);
         $stmt->execute();
         $result = $stmt->get_result();
         if (!$result) {
