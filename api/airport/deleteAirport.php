28c803573c4afa6efbcc547a5a7adf7ff61c40f5
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-07-16 18:16:51 +0300

    solved api issues

--- api/airport/deleteAirport.php
+++ api/airport/deleteAirport.php
@@ -1,6 +1,12 @@
 <?php
+header('Content-Type: application/json');
+
+// Allow cross-origin requests
+header('Access-Control-Allow-Origin: *'); 
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS'); 
+header('Access-Control-Allow-Headers: Content-Type, Authorization'); 
 require_once '../../config/config.php';
-require_once '../../models/Airport.php';
+require_once '../../models/Flight.php';
 
 $data = json_decode(file_get_contents("php://input"));
 
--- api/airport/getSingleAirport.php
+++ api/airport/getSingleAirport.php
@@ -1,7 +1,12 @@
 <?php
-require_once '../../config/config.php';
-require_once '../../models/Airport.php';
+header('Content-Type: application/json');
 
+// Allow cross-origin requests
+header('Access-Control-Allow-Origin: *'); 
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS'); 
+header('Access-Control-Allow-Headers: Content-Type, Authorization'); 
+require_once '../../config/config.php';
+require_once '../../models/Flight.php';
 if (isset($_GET['airport_id'])) {
     $airport_id = $_GET['airport_id'];
 
