2068d0278bb8911fdf83b424a6407b7c46556d60
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-07-16 22:54:08 +0300

    fixed apis

--- api/airport/updateAirport.php
+++ api/airport/updateAirport.php
@@ -1,13 +1,12 @@
 <?php
+require_once '../../config/config.php';
+require_once '../../models/Airport.php';
 header('Content-Type: application/json');
 
 // Allow cross-origin requests
 header('Access-Control-Allow-Origin: *'); 
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS'); 
-header('Access-Control-Allow-Headers: Content-Type, Authorization'); 
-require_once '../../config/config.php';
-require_once '../../models/Flight.php';
-
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
 $data = json_decode(file_get_contents("php://input"));
 
 if (isset($data->Airport_id) && isset($data->AirportName)) {
