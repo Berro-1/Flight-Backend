4850078203f7c4e7ca83276490714303f4f94e2f
Mohammad Omar Abdullah Al-Issa <135043287+Mhmdomarissa@users.noreply.github.com>
2024-07-16 18:25:52 +0300

    adding some access to  getHotels.php

--- api/hotel/getHotels.php
+++ api/hotel/getHotels.php
@@ -1,4 +1,9 @@
 <?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
 require_once '../../config/config.php';
 require_once '../../models/hotels.php';
 
