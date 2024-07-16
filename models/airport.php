76c63cf46f26415d9c49e52dcc31ddc398688d25
Ali-Hadi-Saker <alihady530@gmail.com>
2024-07-16 11:49:52 +0300

    created airport getid modul

--- models/airport.php
+++ models/airport.php
@@ -8,7 +8,16 @@ Class Airport
         $this->mysqli = $mysqli;
     }
     
-    public function getAirportNames(){
+    public function getAirportIdByName($name){
+        $query = 'select Airport_id from airports where AirportName=?';
+        $stmt = $this->mysqli->prepare($query);
+        if (!$stmt) {
+            throw new Exception("Prepare failed: (" . $this->mysqli->errno . ") " . $this->mysqli->error);
+        }
+        $stmt->bind_param('s', $name);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result;
         
     }
 }
