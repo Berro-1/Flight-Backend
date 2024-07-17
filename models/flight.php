ffaf7aafe57ea6091dcc1ac7d61db61675428569
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-07-17 19:35:57 +0300

    flight api

--- models/flight.php
+++ models/flight.php
@@ -194,5 +194,20 @@ class Flight
         
                                
 }
+public function countFlights() {
+    $query = 'SELECT COUNT(*) as total_flights FROM flights';
+    $stmt = $this->mysqli->prepare($query);
+    
+    if (!$stmt) {
+        return ["message" => "Database error: " . $this->mysqli->error];
+    }
+    
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $data = $result->fetch_assoc();
+    
+    return ["total_flights" => $data['total_flights']];
+}
+
         
 }
