9633571b9074f9423be63218dd2aa0b7722ab15b
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-07-17 18:06:40 +0300

    .

--- api/flight/createFlight.php
+++ api/flight/createFlight.php
@@ -5,19 +5,15 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *'); 
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS'); 
 header('Access-Control-Allow-Headers: Content-Type, Authorization'); 
+
 require_once '../../config/config.php';
 require_once '../../models/Flight.php';
-
-
 
 // Initialize the Flight class
 $flightModel = new Flight($mysqli);
 
-// Get the request data
-$data = json_decode(file_get_contents("php://input"), true);
-
-// Create a new flight
-$response = $flightModel->create($data);
+// Count the flights
+$response = $flightModel->countFlights(); // Ensure you have this method in the Flight model
 
 header('Content-Type: application/json');
 echo json_encode($response);
