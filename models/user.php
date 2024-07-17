30f4ada968a46fd9a93eaa188a08990aed59e085
Berro-1 <hussienberro1@gmail.com>
2024-07-17 14:57:35 +0300

    added extended logic for token

--- models/user.php
+++ models/user.php
@@ -86,7 +86,7 @@ class User
         $admin = $result->fetch_assoc();
 
         if ($admin) {
-            if ($password == $admin['admin_password']) {
+            if (password_verify($password, $admin['admin_password'])) {
                 $payload = [
                     'iat' => time(),
                     'exp' => time() + 3600,
@@ -112,7 +112,7 @@ class User
         $user = $result->fetch_assoc();
 
         if ($user) {
-            if ($password==$user['password']) {
+            if (password_verify($password,$user['password'])) {
                 $payload = [
                     'iat' => time(),
                     'exp' => time() + 3600,
